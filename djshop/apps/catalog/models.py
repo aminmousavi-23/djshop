1bb6e4db2be5013ca58fd6be64c06df5e3a34a57
amin mousavi <mousavia54@gmail.com>
2023-11-14 11:40:10 +0330

    Create Product Model

--- djshop/apps/catalog/models.py
+++ djshop/apps/catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from treebeard.mp_tree import MP_Node
 from djshop.apps.catalog.managers import CategoryQuerySet
+from djshop.libs.db.fields import UppercaseCharField
 
 class Category(MP_Node):
     title = models.CharField(max_length=255, db_index=True)
@@ -112,6 +113,28 @@ class Option(models.Model):
     def __str__(self):
         return self.title
     
+    class Meta:
+        verbose_name = 'Option'
+        verbose_name_plural = 'Option'
+
+
+class Product(models.Model):
+
+    class ProductTypeChoice(models.TextChoices):
+        standalone = 'standalone'
+        parent = 'parent'
+        child = 'child'
+
+    structure = models.CharField(max_length=16, choices=ProductTypeChoice.choices, default=ProductTypeChoice.standalone)
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=128, null=True, blank=True)
+    upc = UppercaseCharField(max_length=24, unique=True, null=True, blank=True)
+    is_public = models.BooleanField(default=True)
+    meta_title = models.CharField(max_length=128, null=True, blank=True)
+    meta_description = models.TextField(null=True, blank=True)
+    
+    slug = models.SlugField(unique=True, allow_unicode=True)
+
     class Meta:
         verbose_name = 'Option'
         verbose_name_plural = 'Option'
---
+++ djshop/libs/db/fields.py
@@ -0,0 +1,16 @@
+from typing import Any
+from django.db import models
+
+
+class UppercaseCharField(models.CharField):
+
+    def from_db_value(self, value, *args, **kwargs):
+        return self.to_python(value)
+
+    
+    def to_python(self, value):
+        val =super().to_python(value)
+        if isinstance(val, str):
+            return val.upper()
+        return val
+    
