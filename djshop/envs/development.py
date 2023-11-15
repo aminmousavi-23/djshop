08f9e9d6f7c3531a19c6b2c82be3219e3b84f309
amin mousavi <mousavia54@gmail.com>
2023-11-15 11:32:25 +0330

    Add Log into setting

--- djshop/envs/development.py
+++ djshop/envs/development.py
@@ -15,3 +15,20 @@ DATABASES = {
         'PASSWORD': 'hamilton23'
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+    }
+}
